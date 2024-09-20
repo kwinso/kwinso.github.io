c3537701f72c5e8512bc80a4edbc34347a06e6e6
kwinso <ruslan.sot70@gmail.com>
2024-09-20 07:44:41 +0300

    fix: change semantics for the navbar

--- uni/2/main.js
+++ uni/2/main.js
@@ -1,3 +1,4 @@
+// Это было совсем необязательно, но мне очень хотелось
 window.onload = function () {
   let footer = document.getElementById("footer-year");
   footer.innerHTML = new Date().getFullYear();
